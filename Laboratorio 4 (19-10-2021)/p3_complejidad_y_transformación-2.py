11f7aef2688fb68d3f0856955ce0c7e21c735622
ItaloCcosccoAlvis <82891635+ItaloCcosccoAlvis@users.noreply.github.com>
2021-10-26 03:03:26 -0500

    Implementación de código en python

--- Laboratorio 4 (19-10-2021)/p3_complejidad_y_transformación-2.py
+++ Laboratorio 4 (19-10-2021)/p3_complejidad_y_transformación-2.py
@@ -11,3 +11,16 @@ function twoSum(array){
     return false
 }
 '''
+# Código convertido a python
+def twoSum(array):
+    for i in range(len(array)):
+        for j in range(len(array)):
+            # Verificación de lo tipos de variable
+            if i != j and array[i] + array[j] == 10 and type(i) == type(j) and type(array[i] + array[j]) == type(10):
+                # i and j mismo tipo de variable, diferentes valores en número
+                return True
+    return False
+
+# Casos de prueba
+array = [1,2,3,8,5]
+print(twoSum(array))
