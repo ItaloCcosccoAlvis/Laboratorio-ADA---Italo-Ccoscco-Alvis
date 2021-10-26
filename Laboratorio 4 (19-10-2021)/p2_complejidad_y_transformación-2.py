3fb243575ed5d156219441509cdbf04293b1962b
ItaloCcosccoAlvis <82891635+ItaloCcosccoAlvis@users.noreply.github.com>
2021-10-26 03:00:43 -0500

    Código JS a transforma e indicar su complejidad

---
+++ Laboratorio 4 (19-10-2021)/p2_complejidad_y_transformación-2.py
@@ -0,0 +1,13 @@
+# 3. What is the time complexity in term of O()
+'''
+function twoSum(array){
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; i++) {
+            if (i !== j && array[i] + array[j] === 10) {
+                return true
+            }
+        }
+    }
+    return false
+}
+'''
