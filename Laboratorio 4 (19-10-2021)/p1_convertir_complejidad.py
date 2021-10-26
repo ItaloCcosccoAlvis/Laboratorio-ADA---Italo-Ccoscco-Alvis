a02d2ab9842758369eb97f181ce68f4add110277
ItaloCcosccoAlvis <82891635+ItaloCcosccoAlvis@users.noreply.github.com>
2021-10-26 02:17:08 -0500

    Caso de prueba para la nueva función lineal

--- Laboratorio 4 (19-10-2021)/p1_convertir_complejidad.py
+++ Laboratorio 4 (19-10-2021)/p1_convertir_complejidad.py
@@ -1,12 +1,12 @@
 # 1. Convert from O(n^2) to O(n)
-def greatestNumber(array):
-    for i in array:
-        isIValTheGreatest = True
-        for i in array:
-            if j > i:
-                isIValTheGreatest = False
-            if isIValTheGreatest:
-                return i
+#def greatestNumber(array):
+#   for i in array:
+#        isIValTheGreatest = True
+#        for i in array:
+#            if j > i:
+#                isIValTheGreatest = False
+#            if isIValTheGreatest:
+#                return i
             
 # Conversión de función O(n^2) to O(n) 
 def greatestNumber(array):
@@ -15,3 +15,11 @@ def greatestNumber(array):
         if array[i] > greatest:
             greatest = array[i]
     return greatest
+
+# casos de prueba
+array = [12,13,1,43,23]
+print(greatestNumber(array))
+
+# casos de prueba
+array = [12,13,1,43,23]
+print(greatestNumber(array))
