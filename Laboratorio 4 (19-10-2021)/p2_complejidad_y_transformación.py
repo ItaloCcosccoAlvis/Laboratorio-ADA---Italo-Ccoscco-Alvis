a6b588b7ed83945fea91033ece3d8a7d98ca1b70
ItaloCcosccoAlvis <82891635+ItaloCcosccoAlvis@users.noreply.github.com>
2021-10-26 02:24:47 -0500

    Código en ruby para convertir a otro lenguaje e indicar su complejidad

---
+++ Laboratorio 4 (19-10-2021)/p2_complejidad_y_transformación.py
@@ -0,0 +1,10 @@
+# 2. What is the time complexity of this code
+# def every_other(array):
+#     array.each_with_index do |number,index|
+#         if index.even?
+#             array.each do  |other_number|
+#                 puts number + other_number
+#             end
+#         end
+#     end
+# end
