c77736a2ad10c27ad9a1d5d2a833406861619cc0
Italo <itamikurumi@gmail.com>
2021-10-30 10:30:00 -0500

    Problema 7 implementación y complejidad

---
+++ Laboratorio 5 (26-10-2021)/P7_complejidad_7.java
@@ -0,0 +1,25 @@
+public class P7_complejidad_7 {
+    public static void main(String[] args) {
+        
+        // Problema 7: ¿Cúal es la complejidad?
+        int n = 10; //variable al azar
+        
+        // Bucle doble
+        for(int i = 1; i < n; i = i * 2){ // O(n)
+           System.out.println(i + " :statement");
+        }
+        // Complejidad: O(log(n))
+        /*
+         1 = 2^0
+         2 = 2^1
+         4 = 2^0
+         .
+         .
+         .
+         k = 2^k
+
+        2^k = n
+        k = log2(n)
+        */
+    }
+}
